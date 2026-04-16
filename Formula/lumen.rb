feb9555af2aa21287b0f242d36c60cb3ab6115bc
Nan Ding - macmini <emersonapplebee@gmail.com>
2026-04-17 00:15:36 +0800

    chore: update Homebrew formula sha256 for v2.1.0 release

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -2,7 +2,7 @@ class Lumen < Formula
   desc "macOS log reviewer with color rendering and large file support"
   homepage "https://github.com/emersonding/lumen-log-viewer"
   url "https://github.com/emersonding/lumen-log-viewer/releases/download/v2.1.0/lumen-2.1.0-arm64.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "7d12edc44c3dbbf810a8af136d6c8a187d8ed2b89233522ff939328dca482349"
   license "MIT"
   version "2.1.0"
 
